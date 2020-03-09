02372209b03fb06266ecf0e34240ff336dccd52c
sungjoonpark3 <sungjoon.park3@gmail.com>
2020-03-09 23:26:27 +0900

    p342 검색이벤트 처리까지완료

--- ex02/src/main/java/org/zerock/domain/Criteria.java
+++ ex02/src/main/java/org/zerock/domain/Criteria.java
@@ -13,6 +13,9 @@ public class Criteria {
 	private int pageNum;
 	private int amount;
 	
+	private String type;
+	private String keyword;
+	
 	public Criteria() {
 		this(1,10);
 	}
@@ -21,5 +24,9 @@ public class Criteria {
 	public Criteria(int pageNum,int amount) {
 		this.pageNum=pageNum;
 		this.amount=amount;
+	}
+	
+	public String[] getTypeArr() {
+		return type == null? new String[] {}:type.split("");
 	}
 }
