f60c73af760b04cef149fcca044d66cb1b7f87d7
sungjoonpark3 <sungjoon.park3@gmail.com>
2020-03-29 20:50:03 +0900

    374p 까지 정리

--- src/main/java/com/zerock/controller/SampleController.java
+++ src/main/java/com/zerock/controller/SampleController.java
@@ -10,10 +10,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zerock.domain.SampleVO;
+import com.zerock.domain.Ticket;
 
 import lombok.extern.log4j.Log4j;
 
@@ -77,5 +81,21 @@ public class SampleController {
 		return result;
 		
 	}
+	//@Pathvariable 어노테이션을 이용해서 URL 상에 경로의 일부를 파라미터로 사용가능하다.
+	//Rest방식에서는 URL 자체에 데이터를 식별할 수 있는 정보들을 표현하는 경우가 많으므로 다양한 방식의 PathVariable이 사용된다.
+	//@PathVariable을 적용하고 싶은 경우에는 { }을 이용해서 변수명을 지정하고 @PathVariable을 이용해서 지정된 이름의 변숫값을 얻을 수 있다.
+	// 단 값을 얻을 떄에는 int double과 같은 기본자료형은 사용 할 수 없다
+	@GetMapping("/product/{cat}/{pid}")
+	public String[] getPath(@PathVariable("cat") String cat,@PathVariable("pid") Integer pid) {
+		return new String[] {"categoy:"+cat,"productid:"+pid};
+	}
 	
+	//@RequestBody는 전달 된 (요청)(Request)의 내용(body)을 이용해서 해당 파라미터의 타입으로 반환을 요구한다.
+	//내부적으로 HttpMessageConvertor 타입의 객체들을 이용해서 다양한 포맷의 입력 데이터를 변환 할 수 있다.
+	//대부분은 JSON 데이터를 서버에 보내서 원하는 타입의 객체로 변환하는 용도로 사용되지만 경우에 따라서는 원하는 포맷의 데이터를 보내고 해석해서 원하는 타입으로 사용한다.
+	@PostMapping("/ticket")
+	public Ticket convert(@RequestBody Ticket ticket) {
+		log.info("Convert.....ticket"+ticket);
+		return ticket;
+	}
 }
---
+++ src/main/java/com/zerock/domain/Ticket.java
@@ -0,0 +1,12 @@
+package com.zerock.domain;
+
+import lombok.Data;
+
+@Data
+public class Ticket {
+
+	private int tno;
+	private String owner;
+	private String grade;
+	
+}
